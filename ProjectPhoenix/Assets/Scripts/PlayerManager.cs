a2f106d52a415978399b262f31ec398b67e7b842
ThorsPowa <nwag7879@gmail.com>
2023-04-07 06:10:41 -0400

    Locomotion
    Added basic locomotion, walking running rolling and sprinting to the game

---
+++ ProjectPhoenix/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace NW
+{
+    public class PlayerManager : MonoBehaviour
+    {
+        InputHandler inputHandler;
+        Animator anim;
+
+        void Start()
+        {
+            inputHandler = GetComponent<InputHandler>();
+            anim = GetComponentInChildren<Animator>();
+        }
+
+
+        void Update()
+        {
+            inputHandler.isInteracting = anim.GetBool("isInteracting");
+            inputHandler.rollFlag = false;
+            inputHandler.sprintFlag = false;
+        }
+    }
+}
